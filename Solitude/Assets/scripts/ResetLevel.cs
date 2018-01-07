51ebba86890aef192d70498ae4ed79a5180cab35
Gallegos, Jose Carlos <josecarlos.gallegos@thermofisher.com>
2018-01-07 12:51:59 -0800

    BossFight Scene and fallingPlatform Script

---
+++ Solitude/Assets/scripts/ResetLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ResetLevel : MonoBehaviour {
+
+	// Use this for initialization
+	public string levelName = "inHeaven";
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D(Collider2D col){
+		SceneManager.LoadScene(levelName);
+
+	}
+
+}
---
+++ Solitude/Assets/scripts/fallingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatform : MonoBehaviour {
+
+	public float timeToFall = 2.0f;
+	private bool onPlatform = false;
+	public float maxGravitySpeed = 1.0f;
+
+	Rigidbody2D platformRigidBody;
+
+		void OnTriggerStay2D(Collider2D other) {
+		onPlatform = true;
+		Debug.Log ("The player is on the falling cloud");
+	} 
+	// Use this for initialization
+	void Awake () {
+		platformRigidBody = GetComponent<Rigidbody2D>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if(onPlatform){
+			timeToFall -= Time.deltaTime;
+			if(timeToFall < 0  && platformRigidBody.gravityScale < maxGravitySpeed){
+				platformRigidBody.gravityScale +=  Time.deltaTime;
+			}
+		}
+
+	
+	}
+}
