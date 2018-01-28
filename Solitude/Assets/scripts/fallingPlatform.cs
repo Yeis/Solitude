0a59c33aa5ee17bf6247366f4e4ba16b1dd492fb
Gallegos, Jose Carlos <josecarlos.gallegos@thermofisher.com>
2018-01-28 12:04:19 -0800

    UPdate repo

--- Solitude/Assets/scripts/fallingPlatform.cs
+++ Solitude/Assets/scripts/fallingPlatform.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class fallingPlatform : MonoBehaviour {
 
-	public float timeToFall = 2.0f;
-	private bool onPlatform = false;
-	public float maxGravitySpeed = 1.0f;
+	public float timeToFall = 2.0f; //How much time until the platform falls 
+	public float maxGravityScale = 1.0f; //maximum amount of gravity that we want 
+	private bool onPlatform = false; //boolean that handle if the player is on the platform 
+	private Rigidbody2D platformRigidBody; //rigidbody Component of the platform
 
-	Rigidbody2D platformRigidBody;
 
-		void OnTriggerStay2D(Collider2D other) {
-		onPlatform = true;
-		Debug.Log ("The player is on the falling cloud");
-	} 
-	// Use this for initialization
 	void Awake () {
 		platformRigidBody = GetComponent<Rigidbody2D>();
 	}
+	void OnTriggerStay2D(Collider2D other) {
+		onPlatform = true;
+		Debug.Log ("The player is on the falling cloud");
+	} 
 	// Update is called once per frame
 	void Update () {
 		if(onPlatform){
@@ -25,7 +24,7 @@ public class fallingPlatform : MonoBehaviour {
 			if(timeToFall < 0  && platformRigidBody.gravityScale < maxGravitySpeed){
 				platformRigidBody.gravityScale +=  Time.deltaTime;
 			}
-		}
+	}
 
 	
 	}
