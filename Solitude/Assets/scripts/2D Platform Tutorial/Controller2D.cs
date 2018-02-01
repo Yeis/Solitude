3a58d7d0ba040a77d33f60c86f47db4215193939
Gallegos, Jose Carlos <josecarlos.gallegos@thermofisher.com>
2018-01-31 22:19:16 -0800

    Yeis branch ep 03

--- Solitude/Assets/scripts/2D Platform Tutorial/Controller2D.cs
+++ Solitude/Assets/scripts/2D Platform Tutorial/Controller2D.cs
@@ -18,6 +18,9 @@ public class Controller2D : MonoBehaviour {
     BoxCollider2D collider;
     RaycastOrigins raycastOrigins;
 
+    public CollisionInfo collisions;
+
+
 	// Use this for initialization
 	void Start () {
         collider = GetComponent<BoxCollider2D>();
@@ -60,6 +63,10 @@ public class Controller2D : MonoBehaviour {
             if (hit){
                 velocity.x = (hit.distance - skinWidth) * directionX;
                 rayLength = hit.distance;
+                
+                collisions.left = (directionX == -1); 
+                collisions.right = (directionX == 1); 
+
             }
 
         }
@@ -78,6 +85,8 @@ public class Controller2D : MonoBehaviour {
             if (hit){
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
+                collisions.above = (directionY == 1); 
+                collisions.below = (directionY == -1);
             }
         }
 
@@ -97,5 +106,15 @@ public class Controller2D : MonoBehaviour {
     struct RaycastOrigins {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
+    }
+    public struct CollisionInfo{
+        public bool above , below;
+        public bool left, right ;
+
+        public void Reset(){
+            above = below = false;
+            left = right = false;
+        }  
+
     }
 }
